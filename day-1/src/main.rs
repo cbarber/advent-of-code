bb4a92b01d1f4644bf35a4973cb3af1d0451b985
Craig Barber <craigb@mojotech.com>
2021-12-01 00:20:06 -0500

    feat: day 1. part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -11,4 +11,13 @@ fn main() {
 
     let count = numbers.windows(2).filter(|item| item[0] < item[1]).count();
     println!("part 1: {}", count);
+
+    let count = numbers
+        .windows(3)
+        .map(|item| item[0] + item[1] + item[2])
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .filter(|item| item[0] < item[1])
+        .count();
+    println!("part 2: {}", count);
 }
