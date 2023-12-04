7d1c839a520b9a75eeb15eb38971fd83c2e74ef9
Craig Barber <craigb@mojotech.com>
2023-12-04 12:46:00 -0500

    feat: day-4 part-2 2023

--- 2023/day-4/src/main.rs
+++ 2023/day-4/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use nom::{
     bytes::complete::tag,
     character::complete::{line_ending, multispace0, multispace1},
@@ -9,21 +11,25 @@ use nom::{
 const INPUT: &str = include_str!("input");
 
 struct Card {
+    #[allow(dead_code)]
     id: u32,
     winning: Vec<u32>,
     actual: Vec<u32>,
 }
 
 impl Card {
-    fn value(&self) -> u32 {
-        match self.actual
+    fn matching(&self) -> u32 {
+        self.actual
             .iter()
             .filter(|&&n| self.winning.contains(&n))
-            .count() as u32 {
-                0 => 0,
-                shift => 2u32.pow(shift - 1)
+            .count() as u32
+    }
 
-            }
+    fn value(&self) -> u32 {
+        match self.matching() {
+            0 => 0,
+            shift => 2u32.pow(shift - 1),
+        }
     }
 }
 
@@ -63,9 +69,38 @@ fn process_1(input: &str) -> u32 {
     cards.1.iter().map(|card| card.value()).sum()
 }
 
-fn process_2(input: &str) -> u32 {
-    unimplemented!()
+#[derive(Debug)]
+struct CopyTracker {
+    inner: VecDeque<u32>,
 }
+
+impl CopyTracker {
+    fn new(size: usize) -> Self {
+        Self {
+            inner: VecDeque::from(vec![1; size]),
+        }
+    }
+
+    fn apply_matching(&mut self, matching: u32) -> Option<u32> {
+        for i in 1..=matching {
+            *self.inner.get_mut(i as usize).unwrap() += self.inner[0];
+        }
+        self.inner.pop_front()
+    }
+}
+
+fn process_2(input: &str) -> u32 {
+    let (_, cards) = parse_cards(input).unwrap();
+    let matches: Vec<u32> = cards.iter().map(|card| card.matching()).collect();
+
+    let mut tracker = CopyTracker::new(matches.len());
+    matches
+        .iter()
+        .fold(0u32, |acc, m| acc + tracker.apply_matching(*m).unwrap())
+        .clone()
+}
+
+// 4, 2, 2, 1, 0, 0
 
 fn main() {
     println!("Part 1: {}", process_1(INPUT));
@@ -87,7 +122,13 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 
 #[test]
 fn test_process_2() {
-    const INPUT: &str = r#"
+    const INPUT: &str = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+
 "#;
-    assert_eq!(0, process_2(INPUT));
+    assert_eq!(30, process_2(INPUT));
 }
