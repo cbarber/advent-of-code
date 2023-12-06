b6d084d773eb549697bae98e6ee1c1fb7c03aeaf
Craig Barber <craigb@mojotech.com>
2023-12-06 09:48:19 -0500

    feat: day-6 part-2 2023

--- 2023/day-6/src/main.rs
+++ 2023/day-6/src/main.rs
@@ -1,6 +1,6 @@
 use nom::{
     bytes::complete::tag,
-    character::complete::{self, line_ending, space1},
+    character::complete::{self, digit1, line_ending, space1},
     multi::separated_list1,
     sequence::delimited,
     IResult,
@@ -8,36 +8,56 @@ use nom::{
 
 const INPUT: &str = include_str!("input");
 
-fn process_1(input: &str) -> u32 {
-    let records = parse_records(input).unwrap().1;
+fn process_1(input: &str) -> u64 {
+    let records = parse_records(input, Kerning::Bad).unwrap().1;
     records.iter().map(|record| record.hold_count()).product()
 }
 
-fn process_2(input: &str) -> u32 {
-    todo!()
+fn process_2(input: &str) -> u64 {
+    let records = parse_records(input, Kerning::Good).expect("input to parse into Record").1;
+    records.iter().map(|record| record.hold_count()).product()
 }
 
 struct Record {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Record {
-    fn hold_count(&self) -> u32 {
+    fn hold_count(&self) -> u64 {
         (0..self.time)
             .into_iter()
             .filter(|time| time * (self.time - time) > self.distance)
-            .count() as u32
+            .count() as u64
     }
 }
 
-fn parse_records(input: &str) -> IResult<&str, Vec<Record>> {
+#[derive(Debug, PartialEq)]
+enum Kerning {
+    Bad,
+    Good,
+}
+
+fn parse_records(input: &str, kerning: Kerning) -> IResult<&str, Vec<Record>> {
     let (input, _) = tag("Time:")(input)?;
-    let (input, times) =
-        delimited(space1, separated_list1(space1, complete::u32), line_ending)(input)?;
+    let (input, times) = if kerning == Kerning::Bad {
+        delimited(space1, separated_list1(space1, complete::u64), line_ending)(input)?
+    } else {
+        let (input, digits) =
+            delimited(space1, separated_list1(space1, digit1), line_ending)(input)?;
+        let times = vec![digits.join("").parse().expect("digits to parse into u64")];
+        (input, times)
+    };
+
     let (input, _) = tag("Distance:")(input)?;
-    let (input, distances) =
-        delimited(space1, separated_list1(space1, complete::u32), line_ending)(input)?;
+    let (input, distances) = if kerning == Kerning::Bad {
+        delimited(space1, separated_list1(space1, complete::u64), line_ending)(input)?
+    } else {
+        let (input, digits) =
+            delimited(space1, separated_list1(space1, digit1), line_ending)(input)?;
+        let distances = vec![digits.join("").parse().expect("digits to parse into u64")];
+        (input, distances)
+    };
 
     let records = times
         .iter()
@@ -71,5 +91,5 @@ fn test_process_2() {
 Distance:  9  40  200
 
 ";
-    assert_eq!(0, process_2(INPUT));
+    assert_eq!(71503, process_2(INPUT));
 }
