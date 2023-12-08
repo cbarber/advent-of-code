6a697e74d83f1eaf75e4faafe8b723a03c594e02
Craig Barber <craigb@mojotech.com>
2023-12-07 22:03:53 -0500

    feat: day-7 part-2 2023

--- 2023/day-7/src/main.rs
+++ 2023/day-7/src/main.rs
@@ -12,14 +12,92 @@ use nom::{
 
 const INPUT: &str = include_str!("input");
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 struct Hand {
     cards: Vec<u8>,
     bid: u32,
 }
 
+const JOKER: u8 = 11u8;
+
+#[derive(Debug, PartialEq)]
+struct HandWithWildcard(Hand);
+
+impl HandWithWildcard {
+    fn new(mut hand: Hand) -> Self {
+        hand.cards.iter_mut().for_each(|c| {
+            if *c == JOKER {
+                *c = 1u8;
+            }
+        });
+        Self(hand)
+    }
+
+    fn rank(&self) -> Kind {
+        let jokers = self.0.cards.iter().filter(|c| **c == 1u8).count();
+
+        if jokers == 0 {
+            return self.0.rank();
+        }
+
+        let rank = match (jokers, self.0.rank()) {
+            (_, Kind::FourOfAKind) => Kind::FiveOfAKind,
+            (_, Kind::FullHouse) => Kind::FiveOfAKind,
+
+            (1, Kind::TwoPair) => Kind::FullHouse,
+            (2, Kind::TwoPair) => Kind::FourOfAKind,
+
+            (1, Kind::ThreeOfAKind) => Kind::FourOfAKind,
+            (3, Kind::ThreeOfAKind) => Kind::FourOfAKind,
+
+            (1, Kind::TwoOfAKind) => Kind::ThreeOfAKind,
+            (2, Kind::TwoOfAKind) => Kind::ThreeOfAKind,
+
+            (1, Kind::OneAKind) => Kind::TwoOfAKind,
+
+            (_, rank) => rank,
+        };
+
+        rank
+    }
+}
+
+impl PartialOrd for HandWithWildcard {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let rank_order = self.rank().partial_cmp(&other.rank());
+        if rank_order == Some(std::cmp::Ordering::Equal) {
+            self.0
+                .cards
+                .iter()
+                .zip(other.0.cards.iter())
+                .find_map(|(c1, c2)| {
+                    let cmp = c1.partial_cmp(c2);
+                    if cmp == Some(std::cmp::Ordering::Equal) {
+                        None
+                    } else {
+                        Some(cmp)
+                    }
+                })
+                .expect("hands to be different")
+        } else {
+            rank_order
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd)]
+enum Kind {
+    OneAKind,
+    TwoOfAKind,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
 impl Hand {
-    fn rank(&self) -> u8 {
+    fn rank(&self) -> Kind {
         let mut counts: Vec<u8> = self
             .cards
             .iter()
@@ -32,13 +110,14 @@ impl Hand {
             .collect();
         counts.sort();
         match counts.as_slice() {
-            [5] => 6,
-            [1, 4] => 5,
-            [2, 3] => 4,
-            [.., 3] => 3,
-            [1, 2, 2] => 2,
-            [.., 2] => 1,
-            _ => 0,
+            [5] => Kind::FiveOfAKind,
+            [1, 4] => Kind::FourOfAKind,
+            [2, 3] => Kind::FullHouse,
+            [1, 1, 3] => Kind::ThreeOfAKind,
+            [1, 2, 2] => Kind::TwoPair,
+            [1, 1, 1, 2] => Kind::TwoOfAKind,
+            [1, 1, 1, 1, 1] => Kind::OneAKind,
+            _ => panic!("invalid hand"),
         }
     }
 }
@@ -68,7 +147,7 @@ impl PartialOrd for Hand {
 fn parse_card(input: &str) -> IResult<&str, u8> {
     alt((
         value(10u8, tag("T")),
-        value(11u8, tag("J")),
+        value(JOKER, tag("J")),
         value(12u8, tag("Q")),
         value(13u8, tag("K")),
         value(14u8, tag("A")),
@@ -99,7 +178,19 @@ fn process_1(input: &str) -> u32 {
 }
 
 fn process_2(input: &str) -> u32 {
-    todo!()
+    let mut hands = parse_input(input)
+        .expect("input to be parsed as Hand structs")
+        .1
+        .iter()
+        .map(|h| HandWithWildcard::new(h.clone()))
+        .collect::<Vec<_>>();
+
+    hands.sort_by(|h1, h2| h1.partial_cmp(h2).expect("hands to be different"));
+    hands
+        .iter()
+        .enumerate()
+        .map(|(i, card)| (i as u32 + 1u32) * card.0.bid)
+        .sum()
 }
 
 fn main() {
@@ -121,6 +212,12 @@ QQQJA 483
 
 #[test]
 fn test_process_2() {
-    const INPUT: &str = "";
-    assert_eq!(0, process_2(INPUT))
+    const INPUT: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+
+";
+    assert_eq!(5905, process_2(INPUT))
 }
