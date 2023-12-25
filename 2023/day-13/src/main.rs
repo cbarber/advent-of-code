ea23bd13d44c4f60b89ee254f247b295443b801e
Craig Barber <craigb@mojotech.com>
2023-12-25 08:55:21 -0500

    feat: day-13 part-2 2023

--- 2023/day-13/src/main.rs
+++ 2023/day-13/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use nom::{
     branch::alt,
     character::complete::{self, line_ending},
@@ -19,47 +17,49 @@ enum Ground {
 
 type Line = Vec<Ground>;
 
+fn single_delta(left: &Line, right: &Line) -> Option<usize> {
+    let mut iter = left.iter().zip(right.iter()).enumerate();
+
+    let first = iter.find_map(|(index, (l, r))| (l != r).then_some(index));
+
+    first.filter(|_| iter.all(|(_, (l, r))| l == r))
+}
+
 #[derive(Debug)]
 struct Pattern {
-    #[allow(dead_code)]
-    grid: HashMap<(usize, usize), Ground>,
-
     rows: Vec<Line>,
     cols: Vec<Line>,
 }
 
 impl Pattern {
-    fn new(rows: Vec<Vec<Ground>>) -> Self {
-        let mut grid = HashMap::new();
+    fn new(rows: Vec<Line>) -> Self {
         let mut cols: Vec<Vec<Ground>> = Vec::new();
 
-        for (row, row_data) in rows.iter().enumerate() {
+        for (_, row_data) in rows.iter().enumerate() {
             for (col, ground) in row_data.iter().enumerate() {
-                grid.insert((row, col), ground.clone());
                 if cols.len() <= col {
                     cols.resize_with(col + 1, Vec::new)
                 }
                 cols.get_mut(col).expect("col").push(ground.clone());
             }
         }
-        Self { grid, rows, cols }
+        Self { rows, cols }
     }
 
-    fn summarize(&self) -> usize {
+    fn summarize(&self) -> Vec<usize> {
         let row_centers = Self::centers(&self.rows);
         let col_centers = Self::centers(&self.cols);
 
         row_centers
             .iter()
             .filter_map(|center| Self::center_count(&self.rows, *center))
-            .max()
-            .unwrap_or(0)
-            * 100
-            + col_centers
-                .iter()
-                .filter_map(|center| Self::center_count(&self.cols, *center))
-                .max()
-                .unwrap_or(0)
+            .map(|s| s * 100)
+            .chain(
+                col_centers
+                    .iter()
+                    .filter_map(|center| Self::center_count(&self.cols, *center))
+            )
+            .collect()
     }
 
     fn centers(lines: &Vec<Line>) -> Vec<(usize, usize)> {
@@ -83,6 +83,66 @@ impl Pattern {
             .zip(lines[center.1..].iter())
             .all(|(l, r)| l == r)
             .then_some(center.0 + 1)
+    }
+
+    fn summarize_smudge(&self) -> Vec<usize> {
+        let smudged_summary = *self
+            .summarize()
+            .first()
+            .expect("summary to exist before considering smudge");
+
+        println!("starting search for smudge: {}", smudged_summary);
+
+        println!("row deltas: {:?}", Self::single_deltas(&self.rows));
+        println!("col deltas: {:?}", Self::single_deltas(&self.cols));
+
+        let result: Vec<usize> = Self::single_deltas(&self.rows)
+            .into_iter()
+            .chain(Self::single_deltas(&self.cols).iter().map(|t| (t.1, t.0)))
+            .flat_map(|(r, c)| {
+                let mut rows = self.rows.clone();
+                if rows[r][c] == Ground::Ash {
+                    rows[r][c] = Ground::Rock;
+                } else {
+                    rows[r][c] = Ground::Ash;
+                }
+                let summary = Pattern::new(rows).summarize();
+                println!("{} {}: {:?}", r, c, summary);
+
+                summary
+                    .into_iter()
+                    .filter(|summary| *summary != smudged_summary)
+            })
+            .collect();
+
+        if result.is_empty() {
+            panic!("no smudge found");
+        }
+
+        println!();
+
+        result
+    }
+
+    fn single_deltas(lines: &Vec<Line>) -> Vec<(usize, usize)> {
+        (2..=lines.len())
+            .step_by(2)
+            .flat_map(|size| {
+                lines
+                    .iter()
+                    .enumerate()
+                    .collect::<Vec<(usize, &Line)>>()
+                    .windows(size)
+                    .filter_map(|w| {
+                        let (left_pos, left) = w.first().expect("first");
+                        let (right_pos, right) = w.last().expect("last");
+                        single_delta(left, right)
+                            .map(|pos| vec![(*left_pos, pos), (*right_pos, pos)])
+                    })
+                    .flatten()
+                    .collect::<Vec<(usize, usize)>>()
+            })
+            .collect()
     }
 }
 
@@ -109,11 +169,18 @@ fn parse(input: &str) -> IResult<&str, Vec<Pattern>> {
 
 fn process_1(input: &str) -> usize {
     let (_input, patterns) = parse(input).expect("patterns to parse");
-    patterns.iter().map(|p| p.summarize()).sum()
+    patterns
+        .iter()
+        .map(|p| *p.summarize().first().expect("summary to exist"))
+        .sum()
 }
 
 fn process_2(input: &str) -> usize {
-    todo!()
+    let (_input, patterns) = parse(input).expect("patterns to parse");
+    patterns
+        .iter()
+        .map(|p| *p.summarize_smudge().first().expect("smudge summary"))
+        .sum()
 }
 
 fn main() {
@@ -145,8 +212,22 @@ fn test_process_1() {
 
 #[test]
 fn test_process_2() {
-    const INPUT: &str = "
+    const INPUT: &str = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
 
 ";
-    assert_eq!(0, process_2(INPUT))
+    assert_eq!(400, process_2(INPUT))
 }
