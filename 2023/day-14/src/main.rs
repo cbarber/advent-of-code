b125a8065ffec423c29ee8d794a610601aa71edc
Craig Barber <craigb@mojotech.com>
2024-12-02 16:58:39 -0500

    chore: lingering 2023 changes

--- 2023/day-14/src/main.rs
+++ 2023/day-14/src/main.rs
@@ -1,5 +1,30 @@
 const INPUT: &str = include_str!("input");
 
+fn process_1(input: &str) -> u32 {
+    todo!()
+}
+
+fn process_2(input: &str) -> u32 {
+    todo!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("part 1: {}", process_1(INPUT));
+    println!("part 2: {}", process_2(INPUT));
+}
+
+#[test]
+fn test_process_1() {
+    const INPUT: &str = "
+
+";
+    assert_eq!(0, process_1(INPUT))
+}
+
+#[test]
+fn test_process_2() {
+    const INPUT: &str = "
+
+";
+    assert_eq!(0, process_2(INPUT))
 }
